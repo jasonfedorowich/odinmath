4b876a73dc36f7c3ed026f8054c53702ad59235c
jasonfedorowich <42303804+jasonfedorowich@users.noreply.github.com>
2024-04-22 22:44:17 -0400

    add matrix2

---
+++ odinmath_lib/include/matrix2.h
@@ -0,0 +1,14 @@
+//
+// Created by Jason Fedorowich on 2024-04-20.
+//
+
+#ifndef ODINMATH_MATRIX2_H
+#define ODINMATH_MATRIX2_H
+
+
+class matrix2 {
+
+};
+
+
+#endif //ODINMATH_MATRIX2_H
---
+++ odinmath_lib/src/matrix2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jason Fedorowich on 2024-04-20.
+//
+
+#include "matrix2.h"
